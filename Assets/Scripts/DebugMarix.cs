d623ac81e475e3912632f98d3cf37365bde05c6a
jean <95246204+Raphael1199@users.noreply.github.com>
2026-04-09 13:35:44 +0400

    changemnt d'algorithme génération de donjon

--- Assets/Scripts/DebugMarix.cs
+++ Assets/Scripts/DebugMarix.cs
@@ -7,20 +7,20 @@ public class DebugMatrix : MonoBehaviour
 {
     public List<List<DungeonData>> listToAffich;
 
-    public DungeonData[][] arrayToAffich;
-    public DungeonData[] arrayIntermediaire;
+    public DungeonData.RoomType[][] arrayToAffich;
+    public DungeonData.RoomType[] arrayIntermediaire;
 
     public void SetListToAffich(DungeonData[,] rooms, int width, int height)
     {
-        arrayToAffich = new DungeonData[width][];
+        arrayToAffich = new DungeonData.RoomType[width][];
         for (int x = 0; x < width; x++)
         {
-            arrayIntermediaire = new DungeonData[width];
+            arrayIntermediaire = new DungeonData.RoomType[width];
             for (int y = 0; y < height; y++)
             {
-                arrayIntermediaire[y] = rooms[x,y];
+                    arrayIntermediaire[y] = rooms[x, y].GetRoomType(); 
 
-                Debug.Log(rooms[x, y].GetRoomType());
+                //Debug.Log(rooms[x, y].GetRoomType());
             }
             arrayToAffich[x] = arrayIntermediaire;
         }
--- Assets/Scripts/DungeonManager.cs
+++ Assets/Scripts/DungeonManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
@@ -12,12 +13,17 @@ public class DungeonManager : MonoBehaviour
     private DungeonData[,] map;
     private Vector2Int startPos;
 
+    [SerializeField]
+    private List<Vector2Int> activeRooms = new List<Vector2Int>();
+
     Vector2Int[] directions = {
     new Vector2Int(1, 0) ,  // droite
     new Vector2Int(-1, 0), // gauche
     new Vector2Int(0, 1),  // haut
     new Vector2Int(0, -1) // bas
 };
+
+    public GameObject roomToSpawn;
 
     //debug
     public DebugMatrix DebugMatrix;
@@ -30,12 +36,14 @@ public class DungeonManager : MonoBehaviour
         {
             for (int j = 0; j < depth; j++)
             {
+                //print(i + "///" + j);
                 map[i, j] = new DungeonData();
             }
         }
 
         startPos = new Vector2Int(0, 0);
-        GenerateMap(startPos, startPos, 1);
+        //StartCoroutine(GenerateMap(startPos, startPos, 1, 0));
+        StartCoroutine(GenerateMapNew(startPos, 10));
     }
 
     // Update is called once per frame
@@ -44,26 +52,138 @@ public class DungeonManager : MonoBehaviour
 
     }
 
-    private void GenerateMap(Vector2Int current, Vector2Int previous, int branchChance)
+    private IEnumerator GenerateMap(Vector2Int current, Vector2Int previous, int branchChance, int currentCount)
     {
-        map[current.x, current.y].setIsVisited(true);
-        map[current.x, current.y].SetRoomType(DungeonData.RoomType.Normal);
-
-        Vector2Int next;
-
-        do
+        int count = currentCount++;
+        if (map[current.x, current.y].getIsVisited() == false)
         {
-            next = GetUnvisitedCells(current);
-            if (next.x != -1)
-            {
-                GenerateMap(next, current, branchChance);
-            }
-        }
-        while (next.x != -1);
+            map[current.x, current.y].setIsVisited(true);
+            map[current.x, current.y].SetRoomType(DungeonData.RoomType.Normal);
 
-        DebugMatrix.SetListToAffich(map, width, depth);
+            GameObject createdRoom = Instantiate(roomToSpawn, new Vector3(current.x * 60, 0, current.y * 60), Quaternion.identity);
+            createdRoom.name = currentCount.ToString();
+
+            yield return new WaitForSeconds(2f);
+
+            Vector2Int next;
+
+            do
+            {
+                next = GetUnvisitedCells(current);
+                Debug.Log("Je suis en " + current.x + " / " + current.y + " -> et je génère en -> " + next.x + " / " + next.y);
+                if (next.x != -1)
+                {
+                    StartCoroutine(GenerateMap(next, current, branchChance, currentCount));
+                    //GenerateMap(next, current, branchChance);
+                }
+            }
+            while (next.x != -1);
+
+            DebugMatrix.SetListToAffich(map, width, depth);
+        }
     }
 
+    private IEnumerator GenerateMapNew(Vector2Int start, int maxRoomCount)
+    {
+        Vector2Int current = start;
+        //activeRooms.Add(current);
+
+        int currentCount = 0;
+
+        while(currentCount < maxRoomCount)
+        {
+            yield return new WaitForSeconds(2f);
+            Vector2Int dir = GetRandomDirection(current);
+
+            if (dir.x != -1 && dir.y != -1)
+            {
+                Vector2Int next = dir;
+
+                /////////////////////////////////////////////////////////////////
+                if (IsInsideGrid(next))
+                {
+                    bool roomExists = map[next.x, next.y] != null;
+                    if (roomExists)
+                    {
+
+                        map[next.x, next.y] = new DungeonData();
+                        map[next.x, next.y].SetRoomType(DungeonData.RoomType.Normal);
+
+                        activeRooms.Add(next);
+                        current = next;
+
+                        Debug.Log("Je suis en " + current.x + " / " + current.y + " -> et je génère en -> " + next.x + " / " + next.y);
+                        GameObject createdRoom = Instantiate(roomToSpawn, new Vector3(current.x * 60, 0, current.y * 60), Quaternion.identity);
+                        createdRoom.name = currentCount.ToString();
+                        map[current.x, current.y].setIsVisited(true);
+                        currentCount++;
+                    }
+                }
+            }
+            else
+            {
+                // On repart d'une salle existante --> évite les blocages
+                print("blocage");
+                current = activeRooms[Random.Range(0, activeRooms.Count - 1)];
+            }
+        }
+        //yield return new WaitForEndOfFrame();
+    }
+
+    private Vector2Int GetRandomDirection(Vector2Int currentCell)
+    {
+
+        int x = currentCell.x;
+        int y = currentCell.y;
+
+        List<Vector2Int> allDirections = new List<Vector2Int>();
+
+        if (x + 1 < width)
+        {
+            DungeonData cellToRight = map[x + 1, y];
+
+            if (cellToRight.getIsVisited() == false && IsInsideGrid(currentCell + directions[0]))
+            {
+                allDirections.Add(directions[0]);
+            }
+        }
+
+        if (x - 1 >= 0)
+        {
+            DungeonData cellToLeft = map[x - 1, y];
+
+            if (cellToLeft.getIsVisited() == false && IsInsideGrid(currentCell + directions[1]))
+            {
+                allDirections.Add(directions[1]);
+            }
+        }
+
+        if (y + 1 < depth)
+        {
+            DungeonData cellToFront = map[x, y + 1];
+
+            if (cellToFront.getIsVisited() == false && IsInsideGrid(currentCell + directions[2]))
+            {
+                allDirections.Add(directions[2]);
+            }
+        }
+
+        if (y - 1 >= 0)
+        {
+            DungeonData cellToBack = map[x, y - 1];
+
+            if (cellToBack.getIsVisited() == false && IsInsideGrid(currentCell + directions[0]))
+            {
+                allDirections.Add(directions[3]);
+            }
+        }
+
+        if (allDirections.Count == 0)
+        {
+            return new Vector2Int(-199, -199);
+        }
+        return allDirections[Random.Range(0, allDirections.Count)] + currentCell;
+    }
 
     private Vector2Int GetUnvisitedCells(Vector2Int currentCell)
     {
@@ -84,7 +204,7 @@ public class DungeonManager : MonoBehaviour
 
         if (x - 1 >= 0)
         {
-            var cellToLeft = map[x - 1, y];
+            DungeonData cellToLeft = map[x - 1, y];
 
             if (cellToLeft.getIsVisited() == false)
             {
@@ -94,7 +214,7 @@ public class DungeonManager : MonoBehaviour
 
         if (y + 1 < depth)
         {
-            var cellToFront = map[x, y + 1];
+            DungeonData cellToFront = map[x, y + 1];
 
             if (cellToFront.getIsVisited() == false)
             {
@@ -104,7 +224,7 @@ public class DungeonManager : MonoBehaviour
 
         if (y - 1 >= 0)
         {
-            var cellToBack = map[x, y - 1];
+            DungeonData cellToBack = map[x, y - 1];
 
             if (cellToBack.getIsVisited() == false)
             {
@@ -114,6 +234,7 @@ public class DungeonManager : MonoBehaviour
 
         if (allDirections.Count == 0)
         {
+            Debug.Log("zehbi y a pas la place");
             return new Vector2Int(-1, -1);
         }
         return allDirections[Random.Range(0, allDirections.Count)] + currentCell;
